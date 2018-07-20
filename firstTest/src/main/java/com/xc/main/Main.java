60c43dddcf726cc9690d68c5d729d6f4a3eda60a
LE-xcj <865263907@qq.com>
2018-07-20 12:13:47 +0800

    first

---
+++ firstTest/src/main/java/com/xc/main/Main.java
@@ -0,0 +1,10 @@
+package com.xc.main;
+
+import com.xc.pojo.User;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        System.out.println("xcj");
+    }
+}
