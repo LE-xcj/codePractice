e91819e46bdb6a6d7302a5dc3c46f93116e0afb9
LE-xcj <865263907@qq.com>
2018-07-22 17:14:21 +0800

    add a teacher after reset secondTest version

---
+++ secondTest/src/main/java/com/xc/pojo/Teacher.java
@@ -0,0 +1,22 @@
+package com.xc.pojo;
+
+public class Teacher {
+    private String tname;
+    private String psw;
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+}
